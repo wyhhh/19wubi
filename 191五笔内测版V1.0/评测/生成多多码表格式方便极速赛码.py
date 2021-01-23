698290ab3c6a401f3004140ad4054eab88dabbca
暮歌丶 <329167139@qq.com>
2021-01-23 17:45:17 +0800

    Update 生成多多码表格式方便极速赛码.py

--- 191五笔内测版V1.0/评测/生成多多码表格式方便极速赛码.py
+++ 191五笔内测版V1.0/评测/生成多多码表格式方便极速赛码.py
@@ -1,7 +1,15 @@
+emoji_set = set()
+
+with open('../emoji.txt') as f:
+    for ln in f:
+        sp = ln.split()
+        emoji_set.add(sp[1])
+
 with open('../191五笔码表.txt') as f:
     with open('191五笔多多格式码表.txt', 'w', encoding='gbk') as out:
         for ln in f:
             sp = ln.split()
             code = sp[0]
             for ci in sp[1:]:
+                if ci in emoji_set: break
                 out.write(ci + '\t' + code + '\n')
